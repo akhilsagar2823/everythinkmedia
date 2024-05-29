5fb45f1061d0d868114f05111acd97d1725f6d0e
akhil2823 <akhilsagar2003@gmail.com>
2024-05-29 18:36:51 +0530

    Updated Project page and added Logo (home button) on Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,12 @@ import './Header.css'
 import { Link, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { getLinkData } from '../Redux/generalSlice';
+import logo from '../Assets/homeLogo.png'
+import useWindowWidrh from '../hooks/useWindowWidrh';
 export const Header = () => {
   const location = useLocation().pathname;
   const dispatch = useDispatch();
+  const windowWidth = useWindowWidrh();
   const HeaderFocused = useSelector((state)=>state.general.links); 
   console.log("HeaderFocused",HeaderFocused);
   console.log("location",location);
@@ -15,13 +18,17 @@ export const Header = () => {
   return (
     <div className='Header'>
       <header>
-        <nav>
+        <nav className='mainHeader'> 
+          <div className='Header-logo'>
+            <Link to='/'><img src={logo} alt='logo' /></Link>
+          </div>
           <ul className='Header-content'>
             <li className='Header-item'><Link className={HeaderFocused.project?"Header-item-Link-focused":'Header-item-Link'} to='/project'>Project</Link></li>
             <li className='Header-item'><Link className={HeaderFocused.products?"Header-item-Link-focused":'Header-item-Link'} to='/products'>Products</Link></li>
             <li className='Header-item'><Link className={HeaderFocused.ourTeam?"Header-item-Link-focused":'Header-item-Link'} to='/ourteam'>Our Team</Link></li>
             <li className='Header-item'><Link className={HeaderFocused.contactus?"Header-item-Link-focused":'Header-item-Link'} to='/contactus'>Contact Us</Link></li>
           </ul>
+        {windowWidth > 832 ?<div></div>:""}
         </nav>
       </header>
     </div>
--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -4,50 +4,62 @@ import Desc from '../Assets/Project/projectMainImage.png'
 import BrandingImage2 from '../Assets/Project/projectBranding1.png'
 import BrandingImage1 from '../Assets/Project/projrctBranding2.png'
 import useWindowWidrh from '../hooks/useWindowWidrh'
+import { useGetProjectQuery } from '../../features/projectSlice'
 export const Project = () => {
+  const {data:dataProject} = useGetProjectQuery();
+  const projectData = dataProject?.data;
+  console.log("projectData",projectData); 
   const windowWidth = useWindowWidrh();
   return (
     <div className='Project-section'>
       <div className="Common-BackGround">
         <div className="container">
-          <div className="Desc-Container">
-            <div className="ThumbnailImage-Section">
-              {windowWidth < 992 ? <div className="Desc-Section">
-              <h1 className='DescHeading'>Thumbnail <span style={{ color: "rgb(14, 128, 234)" }}>Design</span></h1>
-              <div className="Desc-Description">Let us craft your perfect Desc to captivate your audience at first glance. Our team will ensure your content stands out</div>
-            </div> 
-            : ""}
-              <div className="Desc-Image"><img className='ThumbnailImg' src={Desc} alt={Desc} /></div>
-              <div className="Thumbnail-Info">
-                <p>Youtube / Desi Games</p>
-                <p>11 Million Views</p>
-                <p>12.5% CTR</p>
+          {projectData?.map((data, index) => {
+            return (
+              <div>
+                {((index + 1)%2)?  <div> <div className="Desc-Container">
+                  <div className="ThumbnailImage-Section">
+                    {windowWidth < 992 ? <div className="Desc-Section">
+                      <h1 className='DescHeading'>{data.name} <span style={{ color: "rgb(14, 128, 234)" }}></span></h1>
+                      <div className="Desc-Description">{data.description}</div>
+                    </div>
+                      : ""}
+                    <div className="Desc-Image"><img className='ThumbnailImg' src={data?.image_url} alt={Desc} /></div>
+                  </div>
+                  {windowWidth < 992 && <div className="Desc-extraInfo py-3"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> {data.project_type}</div>}
+                  {windowWidth > 992 ? <div className="Desc-Section">
+
+                    <h1 className='DescHeading'>{data.name}<br /><span style={{ color: "rgb(14, 128, 234)" }}></span></h1>
+                    <div className="Desc-Description">{data.description}</div>
+                    <div className="Desc-extraInfo"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> {data.project_type}</div>
+                    {windowWidth < 992 && <div className="Desc-extraInfo py-3"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> {data.project_type}</div>}
+                  </div> : ""
+                  }
+                </div>
+              </div>:<div>   
+                  <div className="BrandingLogo-Container">
+                  <div className="Desc-Section">
+                    <h1 className='DescHeading'>{data.name} <br /><span style={{ color: "rgb(14, 128, 234)" }}></span></h1>
+                    <div className="Desc-Description">{data.description}</div>
+                    {windowWidth > 992 && <div className="Desc-extraInfo"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> {data.project_type}</div>}
+                  </div>
+                  <div className="BrandingImage-section">
+                    <div className="BrandingImage1"><img src={data?.image_url} className='ThumbnailImg' alt="" /></div>
+                    
+                  </div>
+                </div>
+                </div>
+                 
+                }
+              
+                
               </div>
-            </div>
-            {windowWidth < 992 && <div className="Desc-extraInfo py-3"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> Avg Ctr from Desc we make is 10% effective</div>}
-            {windowWidth > 992 ? <div className="Desc-Section">
-            
-              <h1 className='DescHeading'>Thumbnail <br /><span style={{ color: "rgb(14, 128, 234)" }}>Design</span></h1>
-              <div className="Desc-Description">Let us craft your perfect Desc to captivate your audience at first glance. Our team will ensure your content stands out</div>
-              <div className="Desc-extraInfo"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> Avg Ctr from Desc we make is 10% effective</div>
-            </div>: ""
-            }
-          </div>
-          <div className="BrandingLogo-Container">
-            <div className="Desc-Section">
-              <h1 className='DescHeading'>Branding <br /><span style={{ color: "rgb(14, 128, 234)" }}>Logo Design</span></h1>
-              <div className="Desc-Description">Let us craft your perfect Branding Logo to captivate your audience at first glance. Our team will ensure your content stands out</div>
-              {windowWidth > 992 && <div className="Desc-extraInfo"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> Avg Ctr from Branding Logo we make is 10% effective</div>}
-            </div>
-            <div className="BrandingImage-section">
-              <div className="BrandingImage1"><img src={BrandingImage1} className='ThumbnailImg' alt="" /></div>
-              <div className="BrandingImage2"><img src={BrandingImage2} className='ThumbnailImg' alt="" /></div>
-            </div>
-            {windowWidth < 992 && <div className="Desc-extraInfo py-3"><span style={{ color: "rgb(14, 128, 234)" }}>Did you know ?</span> <br /> Avg Ctr from Branding Logo we make is 10% effective</div>}
-          </div>
-        </div>
+              
+                )
+          })}        
       </div>
     </div>
+  </div>
 
   )
 }
